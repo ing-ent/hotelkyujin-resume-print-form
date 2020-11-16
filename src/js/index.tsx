595c7bb847c644a3072b28e1331974915949fcd4
Yuya Tanami <yuyatanami@gmail.com>
2020-11-16 15:31:14 +0900

    コンテナ読み込み

---
+++ src/js/index.tsx
@@ -0,0 +1,11 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import 'isomorphic-fetch';
+import * as React from 'react';
+import { render } from 'react-dom';
+// @ts-ignore
+import Root from './Containers/Root';
+
+render(<Root code="" state="" clientId="" />, (document.getElementById('root')));
