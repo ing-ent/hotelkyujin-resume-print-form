00f125c58990cba4c7bd55024428dd4440ac9e2f
Yuya Tanami <yuyatanami@gmail.com>
2020-11-16 15:33:11 +0900

    resume footer

---
+++ src/js/Components/ResumeFooter.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function ResumeFooter() {
+  return (
+    <section className="resume-footer">
+      <img src="/assets/images/logo.gif" />
+    </section>
+  )
+}
